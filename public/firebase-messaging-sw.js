972d8ae14942c7349f6a3318a98caaac6c7bfc82
The boss <duaneqhodges@gmail.com>
2025-06-22 15:22:16 -0400

    Finished chat

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,36 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js"
+);
+
+// Initialize Firebase in the service worker by passing in only the messagingSenderId
+firebase.initializeApp({
+  apiKey: "AIzaSyCbckV9cMuKUM4ZnvYDJZUvfukshsZfvM0",
+  authDomain: "mundo1-dev.firebaseapp.com",
+  projectId: "mundo1-dev",
+  storageBucket: "mundo1-dev.firebasestorage.app",
+  messagingSenderId: "296095212837",
+  appId: "1:296095212837:web:6fd8f831e3d7f642f726cc",
+  measurementId: "G-ZNYVKS2SBF",
+});
+
+// Retrieve an instance of Firebase Messaging
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification.title || "Voyager";
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon-192x192.png", // Make sure this icon exists in your public folder
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
